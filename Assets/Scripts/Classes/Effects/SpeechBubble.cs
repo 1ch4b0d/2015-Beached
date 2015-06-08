badfb2c21cbb96fa4805de5ab89a306fcce2e2a7
Hugbot <Hugbot@Hugbots-MBP.netgear.com>
2015-06-07 18:59:05 -0700

    More directory changes. Beginning placeholder creation of speech bubbles.

---
+++ Assets/Scripts/Classes/Effects/SpeechBubble.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpeechBubble : MonoBehaviour {
+	// Use this for initialization
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+	
+	protected void UpdateAnimator() {
+	}
+}
---
+++ Assets/Scripts/Player/BasicPlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicPlayerController : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        PerformInputLogic();
+    }
+    
+    // TODO: Extract movement logic to its own caluclation and instead use this
+    //       method to for detecting player input
+    protected void PerformInputLogic() {
+        Vector3 movementOffset = Vector3.zero;
+        float movementSpeed = 5f;
+        
+        // Up
+        if(Input.GetKey(KeyCode.W)) {
+            movementOffset.y += movementSpeed;
+        }
+        // Right
+        if(Input.GetKey(KeyCode.D)) {
+            movementOffset.x += movementSpeed;
+        }
+        // Down
+        if(Input.GetKey(KeyCode.S)) {
+            movementOffset.y -= movementSpeed;
+        }
+        // Left
+        if(Input.GetKey(KeyCode.A)) {
+            movementOffset.x -= movementSpeed;
+        }
+        
+        movementOffset.z = 0f;
+        movementOffset = movementOffset * Time.deltaTime;
+        
+        this.transform.position += movementOffset;
+    }
+    
+    protected void UpdateAnimator() {
+    }
+}
