ed6f974dc8243108eaa6d35a71889cf3dc0aa761
Hugbot <Hugbot@Hugbots-MBP.netgear.com>
2015-06-09 01:12:47 -0700

    Speech bubble looks like it's implemented and working correctly in terms of basic animations. Need to create more hooks to have it added by default to an object, as well as having it hide... Will need to test that. One concern is that modifying the sprite color does nothing... because of that I need to test to see if it will fade out.

---
+++ Assets/Scripts/Enums/SpeechBubbleImage.cs
@@ -0,0 +1,9 @@
+﻿public enum SpeechBubbleImage {
+    None,
+    DesktopInteractButton,
+    PS3InteractButton,
+    TouchInteractButton,
+    VitaInteractButton,
+    WiiInteractButton,
+    XBoxInteractButton,
+}
