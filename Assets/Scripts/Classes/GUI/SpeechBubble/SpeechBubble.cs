3f4c937fc6ea1953dfb7b2515d0554faface74df
Hugbot <Hugbot@Hugbots-MBP.netgear.com>
2015-06-12 23:49:17 -0700

    Speech bubble works now. Need to set on finished logic, need to add player disable logic, need to add enable player logic.

--- Assets/Scripts/Classes/GUI/SpeechBubble/SpeechBubble.cs
+++ Assets/Scripts/Classes/GUI/SpeechBubble/SpeechBubble.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SpeechBubble : MonoBehaviour {
+    UIPanel rootPanel = null;
+    
     public Animator animatorReference = null;
     public SpeechBubbleImage speechBubbleImage = SpeechBubbleImage.None;
-    public List<UI2DSprite> sprites = null;
     public List<GoTween> tweens = null;
+    public UILabel label = null;
+    
+    public Queue<string> textSet = new Queue<string>();
     
     void Awake() {
         Initialize();
@@ -14,7 +18,7 @@ public class SpeechBubble : MonoBehaviour {
     
     // Use this for initialization
     void Start() {
-		// by default the speech bubble is hidden on start
+        // by default the speech bubble is hidden on start
         Hide(float.Epsilon);
     }
     
@@ -22,6 +26,19 @@ public class SpeechBubble : MonoBehaviour {
     void Update() {
         UpdateAnimator();
         DebugInfo();
+    }
+    
+    protected void Initialize() {
+        if(rootPanel == null) {
+            rootPanel = this.gameObject.GetComponent<UIPanel>();
+        }
+        if(animatorReference == null) {
+            Debug.LogError("animatorReference is null, please set it to a reference");
+        }
+        
+        if(tweens == null) {
+            tweens = new List<GoTween>();
+        }
     }
     
     void DebugInfo() {
@@ -33,30 +50,63 @@ public class SpeechBubble : MonoBehaviour {
             Debug.Log("Showing");
             Show();
         }
+        // Text
+        if(Input.GetKeyDown(KeyCode.V)) {
+            Debug.Log("Setting Text");
+            SetTextSet("testing testing testing");
+        }
     }
     
-    protected void Initialize() {
-        if(animatorReference == null) {
-            Debug.LogError("animatorReference is null, please set it to a reference");
+    public string PopText() {
+        if(textSet.Count > 0) {
+            return textSet.Dequeue();
         }
+        else {
+            return label.text;
+        }
+    }
+    
+    // I don't know if this is necessarily needed, but fuggit you know
+    public void PopTextAndUpdateSpeechBubbleText() {
+        TypewriterEffect typewriterEffect = label.GetComponent<TypewriterEffect>();
         
-        if(sprites == null) {
-            sprites = new List<UI2DSprite>();
+        if(!typewriterEffect.isActive) {
+            if(textSet.Count > 0) {
+                SetSpeechBubbleText(PopText());
+            }
+            else {
+                SetSpeechBubbleText(label.text, false);
+            }
         }
+    }
+    
+    /// <summary>
+    /// This sets the UILabel's text
+    /// </summary>
+    public void SetSpeechBubbleText(string newText, bool resetTypeWriter = true) {
+        TypewriterEffect typewriterEffect = label.GetComponent<TypewriterEffect>();
         
-        if(tweens == null) {
-            tweens = new List<GoTween>();
+        label.text = newText;
+        
+        if(typewriterEffect != null
+            && resetTypeWriter) {
+            typewriterEffect.ResetToBeginning();
         }
+    }
+    
+    /// <summary>
+    /// This sets the text set to be iterated through
+    /// </summary>
+    public void SetTextSet(params string[] newText) {
+        textSet.Clear();
+        foreach(string text in newText) {
+            textSet.Enqueue(text);
+        }
+        PopTextAndUpdateSpeechBubbleText();
     }
     
     public bool IsHidden() {
-        bool foundObjectShowing = false;
-        foreach(UI2DSprite sprite in sprites) {
-            if(sprite.alpha > 0f) {
-                foundObjectShowing = true;
-            }
-        }
-        return !foundObjectShowing;
+        return (rootPanel.alpha > 0f) ? true : false;
     }
     
     protected void UpdateAnimator() {
@@ -78,18 +128,13 @@ public class SpeechBubble : MonoBehaviour {
     
     public SpeechBubble Show(float duration = 1f) {
         ClearTweens();
-        foreach(UI2DSprite sprite in sprites) {
-            tweens.Add(sprite.alphaTo(duration, 1f));
-        }
-        
+        tweens.Add(rootPanel.alphaTo(duration, 1f));
         return this;
     }
     
     public SpeechBubble Hide(float duration = 1f) {
         ClearTweens();
-        foreach(UI2DSprite sprite in sprites) {
-            tweens.Add(sprite.alphaTo(duration, 0f));
-        }
+        tweens.Add(rootPanel.alphaTo(duration, 0f));
         return this;
     }
 }
