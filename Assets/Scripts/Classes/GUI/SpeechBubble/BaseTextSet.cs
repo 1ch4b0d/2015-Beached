dfb87f81165fe8d78dc30c62ddfe99d8afe58758
Hugbot <loganknecht@gmail.com>
2015-08-13 10:11:10 -0700

    minor commit.

---
+++ Assets/Scripts/Classes/GUI/SpeechBubble/BaseTextSet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseTextSet : MonoBehaviour {
+    public List<string> textSet = null;
+    
+    // Use this for initialization
+    protected virtual void Awake() {
+        Awake();
+    }
+    
+    // Use this for initialization
+    protected virtual void Start() {
+        Start();
+    }
+    
+    // Update is called once per frame
+    protected virtual void Update() {
+        Update();
+    }
+    
+    protected virtual void Initialize() {
+        if(textSet == null) {
+            Debug.LogError(this.gameObject.name + " needs its 'textSet' reference to be set in the 'BaseTextSet' Script");
+        }
+    }
+    
+    public virtual List<string> GetTextSet() {
+        return textSet;
+    }
+}
---
+++ Assets/Scripts/Classes/GUI/SpeechBubble/SpeechBubbleTextSet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpeechBubbleTextSet : BaseTextSet {
+    // Use this for initialization
+    protected override void Awake() {
+        base.Awake();
+    }
+    
+    // Use this for initialization
+    protected override void Start() {
+        base.Start();
+    }
+    
+    // Update is called once per frame
+    protected override void Update() {
+        base.Update();
+    }
+    
+    protected override void Initialize() {
+        base.Initialize();
+    }
+}
