80e9516e76fff6f1aee61401b1c6405e75326f39
Hugbot <loganknecht@gmail.com>
2015-08-18 15:32:52 -0700

    Finished the remaining NPC additions. Did not add Gentleman Bum. Will add that in a future branch.

---
+++ Assets/Scripts/Classes/Events/CoreEvents/AutomaticCustomEvent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is used to execute events immediately.
+/// For example on level load you want certain events to trigger. This will
+/// handle that for you.
+/// </summary>
+public class AutomaticCustomEvent : CustomEventObject {
+    // // Use this for initialization
+    // protected override void Awake() {
+    //     base.Awake();
+    // }
+    
+    // Use this for initialization
+    protected override void Start() {
+        FireStartEvents();
+        FireExecuteEvents();
+        FireFinishEvents();
+    }
+    
+    // // Update is called once per frame
+    // protected override void Update() {
+    //     base.Update();
+    // }
+    
+    // // Use this for initialization
+    // protected override void Initialize() {
+    //     base.Initialize();
+    // }
+}
