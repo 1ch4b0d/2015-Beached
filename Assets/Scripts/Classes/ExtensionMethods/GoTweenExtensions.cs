8fd057fdb31b0d770cd3a22510bcf5526f908f59
Hugbot <loganknecht@gmail.com>
2015-08-20 18:38:53 -0700

    Going to upgrade to Unity 5.2 because that has support for a general substate play mechanism

---
+++ Assets/Scripts/Classes/ExtensionMethods/GoTweenExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GoTweenExtensions {
+    public static void AddGoTween(this GameObject currentGameObject, GoTween newGoTween) {
+        GoTweenManager goTweenManager = currentGameObject.GetComponent<GoTweenManager>();
+        if(goTweenManager == null) {
+            goTweenManager = (currentGameObject.AddComponent<GoTweenManager>()).GetComponent<GoTweenManager>();
+            goTweenManager.Add(newGoTween);
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/GoTween/GoTweenManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GoTweenManager : MonoBehaviour {
+    public List<GoTween> goTweens = null;
+    
+    // Use this for initialization
+    protected virtual void Awake() {
+        Initialize();
+    }
+    
+    // Use this for initialization
+    protected virtual void Start() {
+    }
+    
+    // Update is called once per frame
+    protected virtual void Update() {
+        PruneInvalidTweens();
+    }
+    
+    // Update is called once per frame
+    protected virtual void Initialize() {
+        goTweens = new List<GoTween>();
+    }
+    
+    // There has got to be a shorter more concise way to do this
+    protected virtual void PruneInvalidTweens() {
+        List<GoTween> tweensToPrune = new List<GoTween>();
+        foreach(GoTween goTween in goTweens) {
+            if(goTween == null) {
+                tweensToPrune.Add(goTween);
+            }
+        }
+        
+        foreach(GoTween goTween in tweensToPrune) {
+            goTweens.Remove(goTween);
+        }
+    }
+    
+    public void Add(GoTween newGoTween) {
+        goTweens.Add(newGoTween);
+    }
+    
+    public void Destroy() {
+        List<GoTween> tweensToPrune = new List<GoTween>();
+        foreach(GoTween goTween in goTweens) {
+            goTween.destroy();
+            tweensToPrune.Add(goTween);
+        }
+        
+        foreach(GoTween goTween in tweensToPrune) {
+            goTweens.Remove(goTween);
+        }
+    }
+    
+    public void Complete() {
+        List<GoTween> tweensToPrune = new List<GoTween>();
+        foreach(GoTween goTween in goTweens) {
+            goTween.complete();
+            tweensToPrune.Add(goTween);
+        }
+        
+        
+        foreach(GoTween goTween in tweensToPrune) {
+            goTweens.Remove(goTween);
+        }
+    }
+}
