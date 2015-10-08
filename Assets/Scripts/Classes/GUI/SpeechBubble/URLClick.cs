1054b7a269de3ea76dd6fd7ee06a8daa9a36eb0e
Hugbot <loganknecht@gmail.com>
2015-10-08 01:31:32 -0700

    Added URLClick script and added collider to label text.

---
+++ Assets/Scripts/Classes/GUI/SpeechBubble/URLClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class URLClick : MonoBehaviour {
+    // Use this for initialization
+    protected virtual void Awake() {
+    }
+    
+    // Use this for initialization
+    protected virtual void Start() {
+    }
+    
+    // Update is called once per frame
+    protected virtual void Update() {
+    }
+    
+    protected virtual void OnClick() {
+        UILabel lbl = GetComponent<UILabel>();
+        string url = lbl.GetUrlAtPosition(UICamera.lastWorldPosition);
+        Debug.Log("Clicked on: " + url);
+    }
+}
